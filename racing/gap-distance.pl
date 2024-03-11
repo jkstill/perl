94293580d187ef894acfb9c0b70ff8310461eae7
Jared Still <jkstill@gmail.com>
2024-03-10 17:17:08 -0700

    added testing, added avg speed output, added examples

--- racing/gap-distance.pl
+++ racing/gap-distance.pl
@@ -5,7 +5,8 @@ use warnings;
 
 use Getopt::Long;
 
-# simpler method of assigning defaults with Getopt::Long
+use constant FEET_PER_MILE => 5280;
+use constant CAR_LENGTH => 17.1; # feet
 
 my $traceFile='- no file specified';
 my $opLineLen=80;
@@ -13,16 +14,17 @@ my $number=12.34;
 my $help=0;
 my @multOpts=();
 
-my ($timeGap,$trackLength,$avgSpeed,$lapTime,$timeChart);
+my ($timeGap,$trackLength,$avgSpeed,$lapTime,$timeChart,$unitTest);
 
 # speed and distance are in miles
 
 GetOptions (
-	"time-gap=f" => \$timeGap,
-	"track-length=f" => \$trackLength,
-	"avg-speed=f"  => \$avgSpeed,
-	"lap-time=f"  => \$lapTime,
-	"time-chart!" => \$timeChart,
+	"g|time-gap=f" => \$timeGap,
+	"l|track-length=f" => \$trackLength,
+	"a|avg-speed=f"  => \$avgSpeed,
+	"t|lap-time=f"  => \$lapTime,
+	"c|time-chart!" => \$timeChart,
+	"u|test!" => \$unitTest,
 	"z|h|help!" => \$help,
 ) or die usage(1);
 
@@ -34,6 +36,7 @@ $avgSpeed ||= 0;
 $lapTime ||= 0;
 $timeChart ||= 0;
 $help ||= 0;
+$unitTest ||= 0;
 
 my $mode='';  # CHART or GAP(distance)
 
@@ -46,28 +49,38 @@ print qq{
          help: $help 
 } if 0;
 
-
-$mode = ($timeGap+0 && $avgSpeed+0) ? "GAP" : "";
+$mode = ($unitTest) ? "TEST" : "";
+if (!$mode) {  $mode = ($timeGap+0 && $avgSpeed+0) ? "GAP" : ""; }
 if (!$mode) {  $mode = ($timeGap+0 && $lapTime+0 && $trackLength+0) ? "GAP" : ""; }
-if (!$mode) {  $mode = ($timeChart+0 && $avgSpeed+0) ? "CHART" : "";}
-if (!$mode) {  $mode = ($timeChart+0 && $lapTime+0 && $trackLength+0) ? "CHART" : "";}
+if (!$mode) {  $mode = ($timeChart+0 && $avgSpeed+0) ? "CHART" : ""; }
+if (!$mode) {  $mode = ($timeChart+0 && $lapTime+0 && $trackLength+0) ? "CHART" : ""; }
 
 die usage(1) unless $mode;
 
-my $feetPerSecond = getDistancePerSecond($trackLength, $lapTime, $avgSpeed);
+if ( $mode eq 'TEST' ) {
+	print "testing...\n";
 
-#print "feetPerSecond: $feetPerSecond\n";
+	my $exitStatus = test();
 
-if ( $mode eq 'GAP' ) {
+	exit $exitStatus;
 
-	my $carLength = 17.1; # feet
+} elsif ( $mode eq 'GAP' ) {
+
+	my $feetPerSecond = getDistancePerSecond($trackLength, $lapTime, $avgSpeed);
 	my $gapDistance = $feetPerSecond * $timeGap;
-	printf("gap: %6.3f feet\n",$gapDistance);
-	printf("%5.1f car lengths\n" , $gapDistance / $carLength);
+
+	if ($trackLength and $lapTime) {
+		$avgSpeed = getAvgSpeed($trackLength, $lapTime);
+	}
+
+	printf("        gap: %6.3f feet\n",$gapDistance);
+	printf("  avg speed: %3.2f\n", $avgSpeed);
+	printf("car lengths: %5.1f\n" , $gapDistance / CAR_LENGTH);
 	exit;
 
 } elsif ( $mode eq 'CHART' ) {
 
+	my $feetPerSecond = getDistancePerSecond($trackLength, $lapTime, $avgSpeed);
 	my ($d0001, $d0010, $d0100, $d1000) = (
 		$feetPerSecond * 0.001,
 		$feetPerSecond * 0.010,
@@ -75,6 +88,9 @@ if ( $mode eq 'GAP' ) {
 		$feetPerSecond * 1.000
 	);
 
+	# avoid 'printf (...) interpreted as function at ./gap-distance.pl '
+	# it does not make sense - the first printf generates this warning
+	# the second printf does not generate a warning
 	no warnings 'syntax';
 
 	printf (
@@ -88,26 +104,58 @@ qq{
   \n},
   $d0001, $d0010, $d0100, $d1000);
 
+  printf("avg speed: %-3.2f\n\n", $avgSpeed ? $avgSpeed : getAvgSpeed($trackLength,$lapTime));
+
 } else {
 	die "something bad happened\n";
 }
 
+sub getAvgSpeed {
+	my ($trackLength, $lapTime) = @_;
+
+	# lap times are in seconds
+	return (($trackLength )  / $lapTime ) * 3600 ;
+}
 
 sub getDistancePerSecond {
 	my ($trackLength, $lapTime, $avgSpeed) = @_;
 	# average speed should be MPH
 
-	my $feetPerMile = 5280;
 	my $feetPerSecond;
 
 	if ($avgSpeed) {
-		$feetPerSecond = ($avgSpeed * $feetPerMile)	/ 3600;
+		$feetPerSecond = ($avgSpeed * FEET_PER_MILE) / 3600;
 	} else {
-		my $trackLengthInFeet = $trackLength * $feetPerMile;
+		my $trackLengthInFeet = $trackLength * FEET_PER_MILE;
 		$feetPerSecond = ($trackLengthInFeet / $lapTime ) ;
 	}
 
 	return $feetPerSecond;
+}
+
+sub test {
+
+	my $exitStatus=0;
+
+	my $avgSpeed = getAvgSpeed(1,36);
+
+	if ( $avgSpeed == 100) {
+		print "avgSpeed of $avgSpeed ok\n";
+	} else {
+		warn "avgSpeed of $avgSpeed should be 100\n";
+		$exitStatus = $exitStatus ? $exitStatus : 1;
+	}
+
+	my $feetPerSecond = sprintf('%3.2f', getDistancePerSecond(1, 36, 0));
+
+	if ($feetPerSecond == 146.67) {
+		print "feetPerSecond of $feetPerSecond OK\n";
+	} else {
+		warn "feetPerSecond of $feetPerSecond should be 146.67\n";
+		$exitStatus = $exitStatus ? $exitStatus : 1;
+	}
+
+	return $exitStatus;
 }
 
 sub usage {
@@ -148,9 +196,53 @@ if other options are included, the precedence will be in the order shown
   --c|time-chart    print a time chart
   --z|h|help!
   
-examples here:
+examples:
 
-   $basename --file DWDB_ora_63389.trc --op-line-len 120 --mult-opts A --mult-opts B
+$  ./gap-distance.pl --track-length 1 --lap-time 36.0 --time-chart
+
+  distance covered per time duration in seconds:
+
+  0.001: 0.15
+  0.010: 1.47
+  0.100: 14.67
+  1.000: 146.67
+
+avg speed: 100.00
+
+==================================================================
+
+$  ./gap-distance.pl --avg-speed 100 --time-chart
+
+  distance covered per time duration in seconds:
+
+  0.001: 0.15
+  0.010: 1.47
+  0.100: 14.67
+  1.000: 146.67
+
+avg speed: 100.00
+
+$  ./gap-distance.pl --track-length 1 --lap-time 36.0 --time-gap 0.375
+gap: 55.000 feet
+  3.2 car lengths
+
+
+==================================================================
+
+$  ./gap-distance.pl --track-length 1 --lap-time 36.0 --time-gap 0.375
+        gap: 55.000 feet
+  avg speed: 100.00
+car lengths:   3.2
+
+
+==================================================================
+
+$  ./gap-distance.pl --avg-speed 100 --time-gap 0.375
+        gap: 55.000 feet
+  avg speed: 100.00
+car lengths:   3.2
+
+ 
 };
 
 	exit eval { defined($exitVal) ? $exitVal : 0 };
