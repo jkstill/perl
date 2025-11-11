76e2d2ab1664090cc8120169d2f334419c4b60e2
Jared Still <jkstill@gmail.com>
2025-11-10 19:23:21 -0800

    various changes that I did not keep track of

--- racing/rpm_speed.sh
+++ racing/rpm_speed.sh
@@ -1,7 +1,8 @@
 # rpm_speed -aspect_ratio 30 -tire_width 285 -wheel_size 18 -redline 9000 -differential 4.10
 # rpm_speed -aspect_ratio 30 -tire_width 285 -wheel_size 18 -redline 9000 -differential 4.44
-rpm_speed -aspect_ratio 30 -tire_width 335 -wheel_size 18 -redline 9000 -differential 4.77
+rpm_speed -aspect_ratio 30 -tire_width 335 -wheel_size 18 -redline 10000 -differential 4.77
+rpm_speed -aspect_ratio 30 -tire_width 315 -wheel_size 18 -redline 10000 -differential 4.77
 
 #rpm_speed -aspect_ratio 45 -tire_width 245 -wheel_size 16 -redline 9000 -differential 4.10
 # rx8 S2
-rpm_speed -aspect_ratio 45 -tire_width 225 -wheel_size 18 -redline 10000 -differential 4.77 -gearset rx8
+#rpm_speed -aspect_ratio 45 -tire_width 225 -wheel_size 18 -redline 10000 -differential 4.77 -gearset rx8
